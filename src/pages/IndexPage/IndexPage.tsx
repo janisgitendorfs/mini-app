701f59046f8fb7767b3c8f02cc8af08f372ca4b3
Jānis Gitendorfs <janis.gitendorfs@gmail.com>
2025-07-20 16:13:25 +0300

    Trying out external links

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,43 +1,42 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Section, Cell, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
 import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
-import tonSvg from './ton.svg';
-
 export const IndexPage: FC = () => {
+  const platform = getMobilePlatform();
+  const destination = platform === 'ios' ? 'https://apps.apple.com/us/app/age-of-sails/id6738692514' : 'https://play.google.com/store/apps/details?id=com.street.bunny';
+
   return (
     <Page back={false}>
       <List>
-        <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
-          <Link to="/ton-connect">
-            <Cell
-              before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
-              subtitle="Connect your TON wallet"
-            >
-              TON Connect
+        <Section header="Hello">
+          <Link to={destination} target="_blank">
+            <Cell subtitle="Determined by user agent.">
+                <span className="text-bold">Platform: {platform}</span>
             </Cell>
-          </Link>
-        </Section>
-        <Section
-          header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <Link to="/init-data">
-            <Cell subtitle="User data, chat information, technical data">Init Data</Cell>
-          </Link>
-          <Link to="/launch-params">
-            <Cell subtitle="Platform identifier, Mini Apps version, etc.">Launch Parameters</Cell>
-          </Link>
-          <Link to="/theme-params">
-            <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
           </Link>
         </Section>
       </List>
     </Page>
   );
 };
+
+function getMobilePlatform() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // iOS detection
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return 'ios';
+  }
+
+  // Android detection
+  if (/android/i.test(userAgent)) {
+    return 'android';
+  }
+
+  // Fallback to android
+  return 'android';
+}
+
